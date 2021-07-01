d663cac56f361ebcdf5314bd616bfb0b93e38cd4
Adil <md-adil@live.com>
2021-07-01 18:39:42 +0530

    adding file

--- test/use-read-otp.test.jsx
+++ test/use-read-otp.test.jsx
@@ -1,6 +1,6 @@
 import 'babel-regenerator-runtime';
 import React, { useState } from "react";
-import { act, render, waitFor } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import { useReadOTP } from "../lib";
 window.OTPCredential = true;
 
@@ -22,12 +22,20 @@ navigator.credentials = {
 function OTPRead() {
     const [otp, setOTP] = useState('waiting...');
     useReadOTP(setOTP, {onError: (e) => console.error('my error:', e.message)});
-    return <div title="hello">{otp}</div>
+    return <div title="otp">{otp}</div>
 }
 
 it("reading otp", async () => {
     const screen = render(<OTPRead />);
     expect(globalSignal.aborted).toBe(false);
     await waitFor(() => screen.getByText("12345"));
+    expect(globalSignal.aborted).toBe(true);
+});
+
+it("reading otp expecting umount before received", async () => {
+    const screen = render(<OTPRead />);
+    expect(globalSignal.aborted).toBe(false);
+    expect(screen.getByTitle("otp").innerHTML).toBe("waiting...")
+    screen.unmount();
     expect(globalSignal.aborted).toBe(true);
 });
