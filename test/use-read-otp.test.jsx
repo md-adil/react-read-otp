f979f46d5aef7a3adb605d737a8a1adfc7e85511
Adil <md-adil@live.com>
2021-07-01 21:34:51 +0530

    writing test cases

--- test/use-read-otp.test.jsx
+++ test/use-read-otp.test.jsx
@@ -1,20 +1,21 @@
 import 'babel-regenerator-runtime';
 import React, { useState } from "react";
-import { render, waitFor } from "@testing-library/react";
+import { render, waitFor, fireEvent } from "@testing-library/react";
 import { useReadOTP } from "../lib";
 window.OTPCredential = true;
-
 function sleep(n) {
     return new Promise(x => {
         setTimeout(x, n);
     })
 }
-
 let globalSignal;
 navigator.credentials = { 
     get: async function({ signal, otp: { transport } }) {
         globalSignal = signal;
         await sleep(100);
+        if (signal.aborted) {
+            throw new Error('aborted');
+        }
         return Promise.resolve({code: '12345'})
     }
 };
@@ -38,4 +39,68 @@ it("reading otp expecting umount before received", async () => {
     expect(screen.getByTitle("otp").innerHTML).toBe("waiting...")
     screen.unmount();
     expect(globalSignal.aborted).toBe(true);
+});
+
+it("should activate only if enabled", async () => {
+    function Hello() {
+        const [otp, setOTP] = useState('waiting');
+        const [active, setActive] = useState(false);
+        useReadOTP(setOTP, {
+            enabled: active
+        });
+        return <div>
+            <button role="btn" onClick={() => setActive(true)}>Click</button>
+            <div role="otp">{otp}</div>
+        </div>
+    }
+    const screen = render(<Hello />);
+    await sleep(101);
+    expect(screen.getByRole('otp').innerHTML).toBe('waiting');
+    fireEvent.click(screen.getByRole('btn'));
+    waitFor(() => screen.getByText('12345'))
+});
+
+
+it("timeout", async () => {
+    function Hello() {
+        const [otp, setOTP] = useState('waiting');
+        const [err, setError] = useState('');
+        useReadOTP(setOTP, {
+            timeout: 50,
+            onError(e) {
+                setError(e.message);
+            }
+        });
+        return <div>
+            <div role="otp">{otp}</div>
+            <div role="error">{err}</div>
+        </div>
+    }
+    const screen = render(<Hello />);
+    await waitFor(() => screen.getByText('aborted'))
+    await sleep(100);
+    expect(screen.getByRole('otp').innerHTML).toBe('waiting');
+});
+
+it("manually cancel", async () => {
+    function Hello() {
+        const [otp, setOTP] = useState('waiting');
+        const [err, setError] = useState('');
+        const reader = useReadOTP(setOTP, {
+            onError(e) {
+                setError(e.message);
+            }
+        });
+        return <div>
+            <div role="otp">{otp}</div>
+            <div role="error">{err}</div>
+            <button role="btn" onClick={() => reader()}>Cancel</button>
+        </div>
+    }
+    const screen = render(<Hello />);
+    await sleep(50);
+    fireEvent.click(screen.getByRole('btn'));
+    await sleep(51);
+    expect(screen.getByRole('otp').innerHTML).toBe('waiting');
+    await waitFor(() => screen.getByText('aborted'))
 });
